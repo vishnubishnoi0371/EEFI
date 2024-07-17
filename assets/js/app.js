8dd5351c9cdfc135f1080198cf698809a4ec2fa0
vishnubishnoi0371 <vishnukhicher49@gmail.com>
2024-07-17 10:54:48 +0530

    updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,3 @@
-// document.getElementById("menuIcon").addEventListener("click", function () {
-//     document.querySelector(".nav-flip").classList.toggle("fix-lft");
-//     document.querySelector("label[id='menuIcon']").classList.toggle("menu-open");
-// });
 
 document.addEventListener('DOMContentLoaded', function () {
     const dropdownToggle = document.querySelector('.dropdown-toggle');
@@ -16,5 +12,29 @@ document.addEventListener('DOMContentLoaded', function () {
         if (!dropdown.contains(event.target) && dropdown.classList.contains('open')) {
             dropdown.classList.remove('open');
         }
+    });
+});
+$('.slider').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+});
+
+document.getElementById("menuIcon").addEventListener("click", function () {
+    this.classList.toggle("crossline1");
+    document.querySelector(".nav_bar").classList.toggle("left_false");
+    document.querySelector(".nav_bar").classList.toggle("left_true");
+    document.querySelector(".nav_bar").classList.toggle("d-none");
+});
+
+document.querySelectorAll(".nav_bar li").forEach(function (li) {
+    li.addEventListener("click", function () {
+        document.getElementById("menuIcon").classList.toggle("crossline1");
+        document.querySelector(".nav_bar").classList.toggle("left_false");
+        document.querySelector(".nav_bar").classList.toggle("left_true");
+        document.querySelector(".nav_bar").classList.toggle("d-none");
     });
 });

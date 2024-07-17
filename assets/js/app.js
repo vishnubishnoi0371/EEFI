951b44bc30b7be959105dbfc3dbf5ad1cee7d9c2
vishnubishnoi0371 <vishnukhicher49@gmail.com>
2024-07-17 10:51:27 +0530

    updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,20 @@
-document.getElementById("menuIcon").addEventListener("click", function () {
-    document.querySelector(".nav-flip").classList.toggle("fix-lft");
-    document.querySelector("label[id='menuIcon']").classList.toggle("menu-open");
+// document.getElementById("menuIcon").addEventListener("click", function () {
+//     document.querySelector(".nav-flip").classList.toggle("fix-lft");
+//     document.querySelector("label[id='menuIcon']").classList.toggle("menu-open");
+// });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const dropdownToggle = document.querySelector('.dropdown-toggle');
+    const dropdown = document.querySelector('.dropdown');
+
+    dropdownToggle.addEventListener('click', function (event) {
+        event.preventDefault();
+        dropdown.classList.toggle('open');
+    });
+
+    document.addEventListener('click', function (event) {
+        if (!dropdown.contains(event.target) && dropdown.classList.contains('open')) {
+            dropdown.classList.remove('open');
+        }
+    });
 });
